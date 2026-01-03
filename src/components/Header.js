022bdf2c2b4a518f870ab2ee08f9bbe9ee2caad2
Chaimcv <chaithanyamcv@gmail.com>
2026-01-03 19:09:56 +0530

    Updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,13 +5,16 @@ const Header = () => {
   return (
     <div className>
       <div className='bg-black h-7'></div>
-      <div className='Header_items p-3 mx-2'>
+      <div className='Header_items p-3 mx-2 flex inline-block'>
         <img src={require("./assets/SHOP.CO.png")} alt="shop.co" />
-         <p><select name="shop" defaultValue="shop">
+         <p><select name="show" defaultValue='shop'>
           <option>Cosmetics</option>
            <option>Clothes</option>
             <option>Food items</option>
           </select>
+          <button>On Sale</button>
+          <button>New Arrivals</button>
+          <button>Brands</button>
         </p>
       </div>
     </div>
