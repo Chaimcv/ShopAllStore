8a74455a9b710494c59bd4c7fd373fecb70b18c3
Chaimcv <chaithanyamcv@gmail.com>
2026-01-03 22:57:57 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,35 +1,33 @@
-import React from 'react'
-
+import React from "react";
 
 const Header = () => {
   return (
-    <div className>
-      <div className='bg-black h-7'></div>
-      <div className='Header_items p-3 mx-2 flex inline-block gap-5'>
+    <>
+      <div className="bg-black h-7"></div>
+      <div className="Header_items p-3 flex grid grid-cols-7 ">
         <img src={require("./assets/SHOP.CO.png")} alt="shop.co" />
-         <p className='items gap-5'>
-          <select name="show" defaultValue='shop'>
-          <option>Cosmetics</option>
-           <option>Clothes</option>
+        <p className="items">
+          <select name="show" defaultValue="shop">
+            <option>Cosmetics</option>
+            <option>Clothes</option>
             <option>Food items</option>
           </select>
           <button>On Sale</button>
           <button>New Arrivals</button>
           <button>Brands</button>
-          <input type='text' placeholder='search'></input>
-           <img src={require("./assets/cart.png")} alt="shop.co" />
-            <img src={require("./assets/Frame (3).png")} alt="shop.co" />
+          <input type="text" placeholder="search"></input>
+          <img src={require("./assets/cart.png")} alt="shop.co" />
+          <img src={require("./assets/Frame (3).png")} alt="shop.co" />
         </p>
       </div>
-      <div className='main'>
-        <div className='shop-text'>
+      <div className="main">
+        <div className="shop-text"></div>
+        <div className="image">
+          <img src={require("./assets/Frame 128.png")} alt="shop.co" />
+        </div>
       </div>
-      <div className='image'>
-         <img src={require("./assets/Frame 128.png")} alt="shop.co" />
-      </div>
-</div>
-    </div>
-  )
-}
+    </>
+  );
+};
 
-export default Header
+export default Header;
