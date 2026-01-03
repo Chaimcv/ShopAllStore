493e411080882fb340be873832edc375e8e4f066
Chaimcv <chaithanyamcv@gmail.com>
2026-01-03 19:04:28 +0530

    updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,14 @@ import React from 'react'
 const Header = () => {
   return (
     <div className>
-      <div className='bg-black h-3'></div>
-      <div className='Header_items'>
-        <p><img src={require("./assets/SHOP.CO.png")} alt="shop.co" />
-
+      <div className='bg-black h-7'></div>
+      <div className='Header_items p-3 mx-2'>
+        <img src={require("./assets/SHOP.CO.png")} alt="shop.co" />
+         <p><select name="shop" defaultValue="shop">
+          <option>Cosmetics</option>
+           <option>Clothes</option>
+            <option>Food items</option>
+          </select>
         </p>
       </div>
     </div>
