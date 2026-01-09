a8ab2504197d552f3c77694057c06b043e0e895f
Chaimcv <chaithanyamcv@gmail.com>
2026-01-09 16:33:45 +0530

    updated

--- src/components/Test.js
+++ src/components/Test.js
@@ -79,7 +79,22 @@ console.log(data,"data");
 
 
       {/* display */}
-    <div className="bg-white flex grid grid-cols-4 mx-[3%] my-[3%]"> 
+      {/* large screen */}
+    <div className="bg-white  hidden lg:grid flex grid grid-cols-4 mx-[3%] my-[3%]"> 
+       {data?.map((items)=>(
+        <div className='card w-[95%] h-[95%] mx-auto bg-[#F2F0F1] mt-[1%]' key={items.id}>
+        <p className='title text-left font-semibold text-base m-[2%]'>{items?.title}</p><br/>
+       <img className="image w-[60%] h-[60%] mx-auto "src={items?.image}></img>
+       <p>₹{items?.price}</p>
+       <Link to={`/details/${items.id}`}>View Details</Link>
+         {/* <Link to="/details">View Details</Link> */}
+ 
+        </div>
+       ))}
+    </div>
+
+      {/* small screen */}
+      <div className="bg-white lg:hidden flex grid grid-cols-2 mx-[3%] my-[3%]"> 
       {data?.map((items)=>(
         <div className='card w-[95%] h-[95%] mx-auto bg-[#F2F0F1] mt-[1%]' key={items.id}>
         <p className='title text-left font-semibold text-base m-[2%]'>{items?.title}</p><br/>
