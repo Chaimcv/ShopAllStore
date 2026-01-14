41e7374ee49ce4f315f93f09ced730354dac7d51
Chaimcv <chaithanyamcv@gmail.com>
2026-01-14 11:37:37 +0530

    updates

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,22 +1,33 @@
 import React, { useEffect, useState } from 'react'
-import axios from 'axios';
+import axios, * as others from 'axios';
+
 
 const Cart = () => {
   const[icart,setCart]=useState([]);
    const url = process.env.REACT_APP_API_URL;
   
   useEffect(()=>{
-    cartitems()
-  })
+    cartitems();
+  },[]);
 
   function cartitems(){
-    const axios = require('axios');
-    axios.get(`${url}/carts`)
-   .then(response =>{ console.log(response.data);
-     setCart(response);});
+  //   const axios = require('axios');
+  //   axios.get(`${url}/carts`)
+  //  .then(response =>{ console.log(response.data);
+  //    setCart(response.data);});
+
+  fetch('https://fakestoreapi.com/carts')
+  .then(response => response.json())
+  .then(data =>{ console.log(data);
+   setCart(data);});
   }
+  console.log(icart,"hhhhhh");
   return (
-    <div>Cart</div>
+    <div>
+      <div className='kk' key={icart.id}>
+        <h3>Title:{icart?.userId}</h3>
+      </div>
+    </div>
   )
 }
 
