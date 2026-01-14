d1b03879ec986660e0212971103a633e8d90e29e
Chaimcv <chaithanyamcv@gmail.com>
2026-01-14 11:05:15 +0530

    updates

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,6 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Cart = () => {
+  const[icart,setCart]=useState([]);
+   const url = process.env.REACT_APP_API_URL;
+  
+  useEffect(()=>{
+    cartitems()
+  })
+
+  function cartitems(){
+    const axios = require('axios');
+    axios.get(`${url}/carts`)
+   .then(response =>{ console.log(response.data);
+     setCart(response);});
+  }
   return (
     <div>Cart</div>
   )
