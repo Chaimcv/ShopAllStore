d212201c5c6773c7fdb94fa71960a9c0abf80669
Chaimcv <chaithanyamcv@gmail.com>
2026-01-21 11:08:45 +0530

    updates

--- src/pages/Adminpage.jsx
+++ src/pages/Adminpage.jsx
@@ -9,34 +9,22 @@ const Adminpage = () => {
      console.log(url,"url");
    const [users, setUsers] = useState([]);
     const navigate=useNavigate();
+
      const Adduser = () => {
     navigate("/addinguser");
   };
 
-//   useEffect(() => {
-//       Displayusers();
-//     }, []);
+  const Edituser=()=>{
 
-//     const Displayusers = () => {
-//     console.log("userpage");
-//     axios.get(`${url}`).then((response) => {
-//       console.log(response.data, "displayuserss");
-//       setUser(response.data);
+  }
+
+//   const Deleteuser=(itemsid)=>{
+// axios.delete("url", { params: { id:itemsid } }).then(response => {
+//       console.log(response);
 //     });
-//   };
 
-//   return (
-//     <div className='bg-violet-800'>
-//        <h1 className='bg-pink-800 font-bold text-red-700'> haiiii</h1>
-//        <button>Add user</button>
-//         {user?.map((uitems)=>{
-//             <div key={uitems.id}>
-//                 <h3>Username:{uitems?.username}</h3> 
-//             </div>
-//       })}
-//   </div> 
-//   )
-// }
+//   }
+
 
 console.log(users,"users");
  useEffect(()=>{
@@ -57,12 +45,14 @@ console.log(users,"users");
    
       {users.map((items) => (
        
-            <div className="bg-blue-600 p-4 border-double" key={items.id}>
+            <div className="bg-blue-600 p-4 border-double" key={items?.id}>
          
-               <h3 className="font-bold" >Username:{items?.username}</h3>
-                <h3 className="font-bold" >email id:{items?.email}</h3>
-                  <button className='Edit'>Edit</button><br />
-                   <button className='Delete'>Delete</button>
+               <h3 className="font-bold" >Name:{items?.name.name}</h3>
+                <h3 className="font-bold" >Age:{items?.age}</h3>
+                 <h3 className="font-bold" >email id:{items?.email}</h3>
+                  <h3 className="font-bold" >Phone number:{items?.pnumber}</h3>
+                  <button className='Edit' onClick={Edituser}>Edit</button><br />
+                   {/* <button className='Delete' onClick={()=>Deleteuser(items.id)}>Delete</button> */}
               
           
            
