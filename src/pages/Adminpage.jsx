392d0a608fb9fc70280993359771d16699eaf401
Chaimcv <chaithanyamcv@gmail.com>
2026-01-27 13:12:58 +0530

    additions

--- src/pages/Adminpage.jsx
+++ src/pages/Adminpage.jsx
@@ -1,91 +1,112 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, Navigate, useNavigate } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, Navigate, useNavigate } from "react-router-dom";
 import "../App.css";
 import "./Adminpage.css";
-import ViewUserDetails from './ViewUserDetails';
-import AddUserForm from '../components/AddUserForm';
+import ViewUserDetails from "./ViewUserDetails";
+import AddUserForm from "../components/AddUserForm";
 
-const Adminpage = () => {  
-  const navigate=useNavigate();                    
-     const baseUrl=process.env.REACT_APP_ADMIN_URL;
-    
-    //  console.log(url,"url");
+const Adminpage = () => {
+  const navigate = useNavigate();
+  const baseUrl = process.env.REACT_APP_ADMIN_URL;
 
-    const [users, setUsers] = useState([]);    //to display users in adminpage
-// const users=[{name: "eeeee",email: "ff@gmail.com",age:"22"},
-//              {name: "fff",email: "ee@gmail.com",age:"23"}];
-   const[formvisible,setFormvisible]=useState(false);         //to set the visibility of form--numbering steps 1
-        console.log(formvisible,"formvisibility false");
+  //  console.log(url,"url");
 
-     const Adduser = () => {                //3- state variable value changed to true,hence form(User) is displayed. goto userpage
-      setFormvisible(true);
-      console.log(formvisible,"formvisibility true");
-    };
+  const [users, setUsers] = useState([]); //to display users in adminpage
+  // const users=[{name: "eeeee",email: "ff@gmail.com",age:"22"},
+  //              {name: "fff",email: "ee@gmail.com",age:"23"}];
+  const [formvisible, setFormvisible] = useState(false); //to set the visibility of form--numbering steps 1
+  console.log(formvisible, "formvisibility false");
 
-  const Deleteuser=(deleteid)=>{
-    console.log(deleteid,"deleteid");
-     const url=`${baseUrl}/users/${deleteid}`;
-      console.log(url,"delete-url");
-axios.delete(url).then(response => {
- 
-      console.log(response,"responsecheck");
+  const Adduser = () => {
+    //3- state variable value changed to true,hence form(User) is displayed. goto userpage
+    setFormvisible(true);
+    console.log(formvisible, "formvisibility true");
+  };
+
+  const Deleteuser = (deleteid) => {
+    console.log(deleteid, "deleteid");
+    const url = `${baseUrl}/users/${deleteid}`;
+    console.log(url, "delete-url");
+    axios.delete(url).then((response) => {
+      console.log(response, "responsecheck");
     });
- alert("Deleted");
-employee();
-  }
+    alert("Deleted");
+    employee();
+  };
 
+  console.log(users, "users");
 
-console.log(users,"users");
+  // console.log(users,"users");
 
-// console.log(users,"users");
-
- useEffect(()=>{
-       employee() 
-    },[])
- const employee = () => {
+  useEffect(() => {
+    employee();
+  }, []);
+  const employee = () => {
     console.log("userpage");
-     const url=`${baseUrl}/users`;
-     console.log(url,"userchecking");
+    const url = `${baseUrl}/users`;
+    console.log(url, "userchecking");
     axios.get(url).then((response) => {
-       
       console.log(response.data, "displayuserss");
       setUsers(response.data);
-    })
-   setFormvisible(false) 
-  }
+    });
+    setFormvisible(false);
+  };
 
   return (
-    <div className='not text-black'>
-     {formvisible&&(
-          <AddUserForm 
-          OnSubmitSuccess={()=>employee}
-           OnCancelBtnClick={()=>setFormvisible(false)}/>                       //4-employee function passed to Form(Userpage)
-        )}
-        
-              {/* 2- on btn click */}
-       <button className='Add-user float-left' onClick={Adduser}>+</button>   
-       {users.length>0?(
-     <div>
-      {users.map((items) => (
-            <div className="user-display" key={items?.id}>
-              <div className='user-images'><img src={items?.userimage}  className='user-image' alt='userimg'></img></div>
-              <div className='user-info'>
-               <h3 className="font-semibold" >Name:{items?.name}</h3>
-                <h3 className="font-semibold" >Age:{items?.age}</h3>
-                     <Link to={`/admin/view/${items.id}`}><button className='View'>View</button></Link><br />
-                   <button className='Delete' onClick={()=>Deleteuser(items.id)}>Delete</button>
-                   </div>
-          </div>
-      ))}
+    <div className="not text-black">
+      {formvisible && (
+        <AddUserForm
+          OnSubmitSuccess={() => employee}
+          OnCancelBtnClick={() => setFormvisible(false)}
+        /> //4-employee function passed to Form(Userpage)
+      )}
+
+      {/* 2- on btn click */}
+      <div className="Admin-heroSection flex content-around">
+        <h1 className="font-extrabold ml-[10%]">ADMIN</h1>
+        <button className="Add-user float-left" onClick={Adduser}>
+          +
+        </button>
       </div>
-       ):(
-<><h2>Users not available</h2></>
-       )}
-      
+
+      {users.length > 0 ? (
+        <div className="Display-usersSection">
+          {users.map((items) => (
+            <div className="card">
+              <div className="user-display" key={items?.id}>
+                <div className="user-images">
+                  <img
+                    src={items?.userimage}
+                    className="user-image"
+                    alt="userimg"
+                  ></img>
+                </div>
+                <div className="user-info">
+                  <h3 className="font-semibold">Name:{items?.name}</h3>
+                  <h3 className="font-semibold">Age:{items?.age}</h3>
+                  <Link to={`/admin/view/${items.id}`}>
+                    <button className="View">View</button>
+                  </Link>
+                  <br />
+                  <button
+                    className="Delete"
+                    onClick={() => Deleteuser(items.id)}
+                  >
+                    Delete
+                  </button>
+                </div>
+              </div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <>
+          <h2>Users not available</h2>
+        </>
+      )}
     </div>
-  )
-  }
+  );
+};
 
-
-export default Adminpage
+export default Adminpage;
