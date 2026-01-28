f59cd0072426133f77e780856bdb3a397bfacde9
Chaimcv <chaithanyamcv@gmail.com>
2026-01-28 19:17:28 +0530

    updates

--- src/pages/ViewUserDetails.jsx
+++ src/pages/ViewUserDetails.jsx
@@ -1,60 +1,66 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react'
-import { Link, useParams } from 'react-router-dom';
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Link, useParams } from "react-router-dom";
 import "../App.css";
 import "./ViewUserDetails.css";
-import User from '../components/AddUserForm';
-import UserForm from '../components/AddUserForm';
-import EditForm from '../components/EditForm';
+import UserForm from "../components/AddUserForm";
+import EditForm from "../components/EditForm";
 
 const ViewUserDetails = () => {
-      const { id } = useParams();
-      const baseUrl=process.env.REACT_APP_ADMIN_URL;
-      const[userdetails,setUserdetails]=useState([ ]);
+  const { id } = useParams();
+  const baseUrl = process.env.REACT_APP_ADMIN_URL;
+  const [userdetails, setUserdetails] = useState([]);
 
-      const[formforEditVisibilty,setFormForEditVisibility]=useState(false);
+  const [formforEditVisibilty, setFormForEditVisibility] = useState(false);
 
-      const Edit=()=>{
-        setFormForEditVisibility(true);
-      }
-   
-console.log(userdetails,"userdetails-console");
- useEffect(()=>{
-        viewusers();
-     },[id])
+  const Edit = () => {
+    setFormForEditVisibility(true);
+  };
+
+  console.log(userdetails, "userdetails-console");
+  useEffect(() => {
+    viewusers();
+  }, [id]);
   const viewusers = () => {
     //  console.log("userdetailspage");
-    const url=`${baseUrl}/users/${id}`;
-     axios.get(url).then((response) => {
-       console.log(response.data, "user-displayed");
-       setUserdetails(response.data);
-     })
-   }
+    const url = `${baseUrl}/users/${id}`;
+    axios.get(url).then((response) => {
+      console.log(response.data, "user-displayed");
+      setUserdetails(response.data);
+    });
+  };
 
   return (
-    <div >
-        {formforEditVisibilty&&(
-            <EditForm 
-            OnEditSuccess={userdetails}
-            EditId={id}/>
-        )}
-    <div key={userdetails?.id}>
-      <div className='HeroSection'>
-         <h2 className="font-semibold" >NAME: {userdetails?.name}</h2>
-         <button className='Edit bg-black text-white' onClick={Edit}>Edit</button>
-      </div>
+    <div>
+      {formforEditVisibilty && (
+        <EditForm OnEditSuccess={userdetails} EditId={id} />
+      )}
+      <div key={userdetails?.id}>
+        <div className="HeroSection">
+          <h2 className="font-semibold">NAME: {userdetails?.name}</h2>
+          <button className="Edit bg-black text-white" onClick={Edit}>
+            Edit
+          </button>
+        </div>
         <div>
-         <div className='viewuserdiv'>
-           <div> <img src={userdetails?.userimage} className='w-[20%] h-[20%]' alt='user-profileimage' /></div>
-          <h3 >Age: {userdetails?.age}</h3>
-         <h2>Phone Number: {userdetails?.pnumber}</h2>
-          <h3 >Email: {userdetails?.email}</h3>
-          <p>Address: {userdetails?.address}</p>
+          <div className="viewuserdiv">
+            <div>
+              {" "}
+              <img
+                src={userdetails?.userimage}
+                className="w-[20%] h-[20%]"
+                alt="user-profileimage"
+              />
+            </div>
+            <h3>Age: {userdetails?.age}</h3>
+            <h2>Phone Number: {userdetails?.pnumber}</h2>
+            <h3>Email: {userdetails?.email}</h3>
+            <p>Address: {userdetails?.address}</p>
           </div>
-          </div>
+        </div>
+      </div>
     </div>
-  </div>  
-  )
-}
+  );
+};
 
-export default ViewUserDetails
+export default ViewUserDetails;
