ca735f7985323b991c90ab8b134fa6a08f2f5137
Chaimcv <chaithanyamcv@gmail.com>
2026-01-29 15:46:45 +0530

    styling

--- src/components/Test.js
+++ src/components/Test.js
@@ -87,7 +87,7 @@ console.log(data,"data");
         <p className='title text-left font-semibold text-base m-[2%]'>{items?.title}</p><br/>
        <img className="image w-[60%] h-[60%] mx-auto "src={items?.image}></img>
        <p>₹{items?.price}</p>
-       <Link to={`/details/${items.id}`}><button className='bg-black text-white m-1 rounded-2xl' >View Details</button></Link><br />
+       <Link to={`/details/${items.id}`}><button className='bg-black text-white m-1 rounded-2xl' >View Details</button></Link>
         <button className='bg-black text-white m-1 rounded-2xl' onClick={()=>Addtocart(items)}>Add to cart</button>
          
  
