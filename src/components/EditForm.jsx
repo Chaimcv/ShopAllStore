47f59abce50fcd28104d720cbfd6609be6e04f7a
Chaithanya <chaithanyamcv@gmail.com>
2026-05-26 23:05:07 +0530

    Update EditForm.jsx

--- src/components/EditForm.jsx
+++ src/components/EditForm.jsx
@@ -15,8 +15,6 @@ const EditForm = ({OnEditSuccess,EditId}) => {
   const [pnumber, setPhonenumber] = useState();
    const [userimage, setUserimage] = useState();
     const [address, setAddress] = useState()
- 
- 
      const Datasadded = {
     name,
     age,
